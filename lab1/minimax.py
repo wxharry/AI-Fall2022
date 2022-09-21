2316968338e443f418cfce49d9d213d3a8ddcac1
Harry <xiaohanwu12@gmail.com>
2022-09-21 15:57:52 -0400

    lab1: add minimax function

--- lab1/minimax.py
+++ lab1/minimax.py
@@ -1,4 +1,5 @@
 import argparse
+from math import inf
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='')
@@ -42,11 +43,52 @@ def find_root(graph):
             parents.add(k)
     return  parents - children
 
+def minimax_helper(s, maxTurn, graph, alpha=None, beta=None, verbose=False):
+    choice = None
+    if isinstance(graph[s], int):
+        return choice, graph[s]
+    if maxTurn:
+        v = - inf
+        for c in graph[s]:
+            [_choice, _v] = minimax_helper(c, not maxTurn, graph, alpha, beta, verbose)
+            if verbose and _choice:
+                print(f"max({c}) chooses {_choice} for {_v}")
+            if _v > v:
+                v = _v
+                choice = c
+            if beta != None and _v >= beta:
+                return None, v
+            if alpha != None and _v > alpha:
+                alpha = _v
+        return choice, v
+    else:
+        v = inf
+        for c in graph[s]:
+            [_choice, _v] = minimax_helper(c, not maxTurn, graph, alpha, beta, verbose)
+            if verbose and _choice:
+                print(f"min({c}) chooses {_choice} for {_v}")
+            if _v < v:
+                v = _v
+                choice = c
+            if alpha != None and _v <= alpha:
+                return None, v
+            if beta != None and _v < beta:
+                beta = _v
+        return choice, v
+
+def minimax(s, maxTurn, graph, ab, verbose=False):
+    if ab:
+        [choice, v] = minimax_helper(s, maxTurn, graph, -inf, inf, verbose=verbose)
+    else:
+        [choice, v] = minimax_helper(s, maxTurn, graph, verbose=verbose)
+    print(f"{'max' if maxTurn else 'min'}({s}) chooses {choice} for {v}")
+    
+
 def main():
     args = parse_arguments()
     graph = read_input(args['graph-file'][0])
     root = find_root(graph)
-    print(root)
+    minimax(list(root)[0], args['min/max'][0] == 'max', graph, args['ab'], verbose=args['v'])
 
 if __name__ != "main":
     main()
